b058ec61a05d91e946dd0ce1aa72a31519ccbef0
Danielle Burrage <burragedanielle@gmail.com>
2020-06-07 17:25:56 -0500

    click images polished functionality

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,12 +2,22 @@ let productImages = [
     'assets/img/charisse-kenion-BgbOGZRmeXs-unsplash.jpg', 'assets/img/eloise-ambursley-59Td06omPVc-unsplash.jpg'
 ]
 
-let currentSlide = 0;
-let slide = productImages[0];
+$('.minor-product-image').click((e) => {
+    var imageId = e.target.id;
+    console.log(imageId);
+    switch (imageId) {
+        case 'm-image-1':
+            $('#main-product-image').attr('src', 'assets/img/charisse-kenion-BgbOGZRmeXs-unsplash.jpg');
+            $('#m-image-1').attr('src', 'assets/img/eloise-ambursley-59Td06omPVc-unsplash.jpg');
+            break;
+        case 'm-image-2':
+            $('#main-product-image').attr('src', 'assets/img/jess-harper-sunday-tBKYocAkzRU-unsplash.jpg');
+            $('#m-image-2').attr('src', 'assets/img/eloise-ambursley-59Td06omPVc-unsplash.jpg');
+            break;
+        case 'm-image-3':
+            $('#main-product-image').attr('src', 'assets/img/the-creative-exchange-J_shueNor80-unsplash.jpg');
+            $('#m-image-3').attr('src', 'assets/img/eloise-ambursley-59Td06omPVc-unsplash.jpg');
+            break;
+    }
 
-console.log(slide);
-
-$('#m-image-1').click(() => {
-    $('#main-product-image').attr('src', 'assets/img/charisse-kenion-BgbOGZRmeXs-unsplash.jpg');
-    $('#m-image-1').attr('src', 'assets/img/eloise-ambursley-59Td06omPVc-unsplash.jpg');
 });
